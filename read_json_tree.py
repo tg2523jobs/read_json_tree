952d13ebae7dff3e964b63c6e91ff41206e34a6e
tg2523jobs <tg25@kimo.com>
2022-10-20 15:56:56 +0800

    move def loop_file

--- read_json_tree.py
+++ read_json_tree.py
@@ -23,27 +23,27 @@ class test_tree:
         print(self.tree_data["project_id"])
         self.loop_file(self.tree_data["file_version"])
 
-def loop_file(NODE_DATA, LEVEL_INDEX = 0):
-    level_index = LEVEL_INDEX
-    node_data = NODE_DATA
+    def loop_file(self, NODE_DATA, LEVEL_INDEX = 0):
+        level_index = LEVEL_INDEX
+        node_data = NODE_DATA
 
-    for node in node_data:
-        # print(node)
-        for node_inside in node:
-            add_head = ""
-            if level_index > 0:
-                add_head = "|   "
-            print("%s|-->%s" % (add_head * level_index, node_inside))
-            if node[node_inside]["filetype"] == "file":
-                if node[node_inside]["project_version"].upper() == "YES":
-                    if "now_version" in node[node_inside]:
-                        print("|   %s|-->now version:%s" % (add_head * level_index, node[node_inside]["now_version"]))
-                    if "now_version" in node[node_inside]:
-                        print("|   %s|-->new version:%s" % (add_head * level_index, node[node_inside]["now_version"]))
-            elif node[node_inside]["filetype"] == "dir":
-                level_index += 1
-                loop_file(node[node_inside]["children"], level_index)
-                level_index -= 1
+        for node in node_data:
+            # print(node)
+            for node_inside in node:
+                add_head = ""
+                if level_index > 0:
+                    add_head = "|   "
+                print("%s|-->%s" % (add_head * level_index, node_inside))
+                if node[node_inside]["filetype"] == "file":
+                    if node[node_inside]["project_version"].upper() == "YES":
+                        if "now_version" in node[node_inside]:
+                            print("|   %s|-->now version:%s" % (add_head * level_index, node[node_inside]["now_version"]))
+                        if "now_version" in node[node_inside]:
+                            print("|   %s|-->new version:%s" % (add_head * level_index, node[node_inside]["now_version"]))
+                elif node[node_inside]["filetype"] == "dir":
+                    level_index += 1
+                    loop_file(node[node_inside]["children"], level_index)
+                    level_index -= 1
 
 
 
